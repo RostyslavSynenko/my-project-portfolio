f32cb1063d9d964bbd5b2c2dc2bb485ce2dbc1c4
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2019-06-11 07:07:49 +0000

    refactoring

--- js/index.js
+++ js/index.js
@@ -1,63 +1,57 @@
-'use strict';
-
-document.querySelector('.show-github-info')
-  .addEventListener('click', fetchGet);
-
-function fetchGet() {
-  fetch('my-data.json')
+{
+  'use strict';
+  
+  document.querySelector('.show-github-info')
+    .addEventListener('click', fetchGet);
+  
+  function fetchGet() {
+    fetch('my-data.json')
+      .then(response => {
+        return response.json();
+      })
+      .then(user => {
+        return fetch(`https://api.github.com/users/${user.name}`);
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(githubUser => {
+        createCover(githubUser);
     
-    .then(response => {
-      const user = response.json();
-      
-      return user;
-    })
+        setTimeout(() => {
+            document.body.removeChild(document.querySelector('.cover-div')); 
+        }, 3000);
+      })
+      .catch(error => {
+          console.error('ERROR', error);
+      });
+  }
   
-    .then(user => {
-      return fetch(`https://api.github.com/users/${user.name}`);
-    })
+  function createCover(user) {
+    const coverDiv = document.createElement('div');
+    coverDiv.className = 'cover-div';
     
-    .then(response => {
-        const githubUser = response.json();
-        
-        return githubUser;
-    })
-
-    .then(githubUser => {
-      createCover(githubUser);
+    const img = document.createElement('img');
+    img.src = user.avatar_url;
+    img.alt = 'GitHub avatar';
+    img.className = 'github-avatar';
+    coverDiv.appendChild(img);
   
-      setTimeout(() => {
-          document.body.removeChild(document.querySelector('.cover-div')); 
-      }, 3000);
-    })
-    .catch(error => {
-        console.log('ERROR', error);
-    });
-}
-
-function createCover(user) {
-  const coverDiv = document.createElement('div');
-  coverDiv.className = 'cover-div';
+    const parag = document.createElement('p');
   
-  const img = document.createElement('img');
-  img.src = user.avatar_url;
-  img.alt = 'GitHub avatar';
-  img.style = 'height: 85%; margin: 5px;';
-  coverDiv.appendChild(img);
-
-  const parag = document.createElement('p');
-
-  parag.innerHTML = `<b>Name:</b> ${user.name}<br>` + 
-    `<b>Created:</b> ${new Date(user.created_at).toLocaleDateString()}<br>` +
-    `<b>Location:</b> ${user.location}<br>` + 
-    `<b>Public repositories:</b> ${user.public_repos}<br>` +
-    `<b>Link:</b> <a href='${user.html_url}'>GitHub</a>`;
-  coverDiv.appendChild(parag);
-
-  document.body.appendChild(coverDiv);
+    parag.innerHTML = `<b>Name:</b> ${user.name}<br>` + 
+      `<b>Created:</b> ${new Date(user.created_at).toLocaleDateString()}<br>` +
+      `<b>Location:</b> ${user.location}<br>` + 
+      `<b>Public repositories:</b> ${user.public_repos}<br>` +
+      `<b>Link:</b> <a href='${user.html_url}'>GitHub</a>`;
+    coverDiv.appendChild(parag);
+  
+    document.body.appendChild(coverDiv);
+  }
+  
+  
+  const clock = document.createElement('div');
+  clock.className = 'clock';
+  document.querySelector('header').appendChild(clock);
+  setInterval( () => clock.innerText = (new Date().toLocaleTimeString()), 1000);
 }
-
-
-const clock = document.createElement('div');
-clock.className = 'clock';
-document.querySelector('header').appendChild(clock);
-setInterval( () => clock.innerText = (new Date().toLocaleTimeString()), 1000);
